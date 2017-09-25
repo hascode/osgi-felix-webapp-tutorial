8176d6f81621407dd17d3685a20826c2ff3d18b6
Micha Kops <micha.kops@gmail.com>
2017-09-25 20:17:43 +0200

    Initial import.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Creating a Web Application using OSGi, Felix, OBR and Maven
+
+----
+
+**2010 Micha Kops / hasCode.com**
---
+++ hello-bundle/src/main/java/com/hascode/bundle/information_broker/api/InformationBroker.java
@@ -0,0 +1,5 @@
+package com.hascode.bundle.information_broker.api;
+
+public interface InformationBroker {
+    public abstract String getInformation();
+}
